de1942dd878a5802c232b2776257b3f2a68a957b
Wijaya <wijayaac25@gmail.com>
2020-11-18 20:48:15 +0800

    state explanation

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            titleColor: 'blue'
+        };
+        this._changeColor = this._changeColor.bind(this);
+    }
+
+    _changeColor() {
+        let { titleColor } = this.state;
+        if (titleColor === 'blue') {
+            this.setState({ titleColor: 'red' });
+        } else {
+            this.setState({ titleColor: 'blue' });
+        }
     }
     render() {
         return (
             <div>
-                <h1>{this.props.title}</h1>
+                <button onClick={this._changeColor}>Ganti Warna</button>
+                <h1 style={{ color: this.state.titleColor }}>{this.props.title}</h1>
                 <span>{this.props.subTitle}</span>
             </div>
         );
