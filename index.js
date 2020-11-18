ebbf8ae873c8d9abe53f0c765f8ea0521fcc9479
Wijaya <wijayaac25@gmail.com>
2020-11-18 21:15:14 +0800

    state for stopwatch

--- index.js
+++ index.js
@@ -1,61 +1,34 @@
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            titleColor: 'blue'
-        };
-        this._changeColor = this._changeColor.bind(this);
-    }
-
-    _changeColor() {
-        let { titleColor } = this.state;
-        if (titleColor === 'blue') {
-            this.setState({ titleColor: 'red' });
-        } else {
-            this.setState({ titleColor: 'blue' });
-        }
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this._changeColor}>Ganti Warna</button>
-                <h1 style={{ color: this.state.titleColor }}>{this.props.title}</h1>
-                <span>{this.props.subTitle}</span>
-            </div>
-        );
-    }
-}
-class Container extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <p> Welcome to Our Tutorial</p>
-                <p> Free Tutorial Coursee</p>
-            </div>);
-    }
-}
-class Footer extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (<span> copyright by wijaya</span>);
-    }
-}
 class Page extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isStarted: false,
+            count: 0
+        }
+        this._start = this._start.bind(this);
+        this._stop = this._stop.bind(this);
+    }
+    _start() {
+        let { count } = this.state;
+        this.countId = setInterval(() => {
+            count = count + 1;
+            this.setState({ count: count });
+        }, 1000);
+        this.setState({ isStarted: true });
+    }
+    _stop() {
+        clearInterval(this.countId);
+        this.setState({ count: 0, isStarted: false });
     }
     render() {
         return (
             <div>
-                <Header title="Kadek Wijaya" subTitle="Tutorial React" />
-                <Container />
-                <Footer />
-            </div>);
+                <h1>My Time</h1>
+                <h1>{this.state.count}</h1>
+                <button onClick={this._start} disabled={this.state.isStarted}>Start</button>
+                <button onClick={this._stop}>Stop</button>
+            </div>
+        );
     }
 }
 
