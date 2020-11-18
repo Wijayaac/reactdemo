d808f8d5ffa60f8fa07e21f9afc1c766973f92df
Wijaya <wijayaac25@gmail.com>
2020-11-18 21:26:54 +0800

    state on handling form

--- index.js
+++ index.js
@@ -2,31 +2,18 @@ class Page extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isStarted: false,
-            count: 0
+            data: ""
         }
-        this._start = this._start.bind(this);
-        this._stop = this._stop.bind(this);
+        this._handleChange = this._handleChange.bind(this);
     }
-    _start() {
-        let { count } = this.state;
-        this.countId = setInterval(() => {
-            count = count + 1;
-            this.setState({ count: count });
-        }, 1000);
-        this.setState({ isStarted: true });
-    }
-    _stop() {
-        clearInterval(this.countId);
-        this.setState({ count: 0, isStarted: false });
+    _handleChange(e) {
+        this.setState({ data: e.target.value });
     }
     render() {
         return (
             <div>
-                <h1>My Time</h1>
-                <h1>{this.state.count}</h1>
-                <button onClick={this._start} disabled={this.state.isStarted}>Start</button>
-                <button onClick={this._stop}>Stop</button>
+                <input type="text" onChange={this._handleChange}></input>
+                <h2>{this.state.data}</h2>
             </div>
         );
     }
